156c49e463936600045f9e7ffae6f3802ec8fb49
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-05-16 15:05:55 +0100

    implement messagelist and styling

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Message = (props) => {
-  return(
-    <div>
-      Hello!
+  const niceDate = new Date(props.message.created_at).toLocaleString("en-GB");
+  return (
+    <div className="message">
+      <p className="message-metadata">
+        <span className="username">{props.message.author}</span>
+        <span className="timestamp">{niceDate}</span>
+      </p>
+      <p>{props.message.content}</p>
     </div>
   );
 };
--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from 'react-redux';
 
+import Message from "../components/message";
+
 class MessageList extends Component { //eslint-disable-line
   render() {
     console.log(this.props.messages);
     return (
       <div className="message-list">
         <h3 className="bottom-grey">Messages</h3>
+        {
+          this.props.messages.map((message) => {
+            return <Message message={message} />;
+          })
+        }
       </div>
     );
   }
