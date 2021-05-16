e3f757d0e2b5f566f81d0cad6a1878e58c861c59
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-05-16 13:58:43 +0100

    add some initial messages to play with

--- src/index.jsx
+++ src/index.jsx
@@ -29,7 +29,18 @@ const middleware = applyMiddleware(logger);
 
 // initialState
 const initialState = {
-  messages: [],
+  messages: [
+    {
+      author: "anonymous92",
+      content: "Hello world!",
+      created_at: "2017-09-26T23:03:16.365Z"
+    },
+    {
+      author: "anonymous77",
+      content: "My name is anonymous77",
+      created_at: "2017-09-26T23:03:21.194Z"
+    }
+  ],
   channels: ["general", "react", "paris"],
   selectedChannel: "general",
   currentUsername: "Deletethis" || window.prompt("What is your username?") ||  `user${Math.floor(Math.random() * 1e6)}` //eslint-disable-line
