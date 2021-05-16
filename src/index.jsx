af1367822f408e7fccaf4005f26c74c44a16a93b
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-05-16 13:26:14 +0100

    init middleware

--- src/index.jsx
+++ src/index.jsx
@@ -2,25 +2,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
 // State and reducers
-// Import the things
+//   Import the things
 import channelsReducer from "./reducers/channels_reducer";
 import currentUsernameReducer from "./reducers/current_username_reducer";
 import messagesReducer from "./reducers/messages_reducer";
 import selectedChannelReducer from "./reducers/selected_channel_reducer";
 
+//   Reduce the things
 const reducers = combineReducers({
   channels: channelsReducer,
   currentUsername: currentUsernameReducer,
   messages: messagesReducer,
   selectedChannel: selectedChannelReducer
 });
+
+// middleware
+const middleware = applyMiddleware(logger);
 
 // initialState
 const initialState = {
@@ -32,7 +37,7 @@ const initialState = {
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, initialState)}>
+  <Provider store={createStore(reducers, initialState, middleware)}>
     <App />
   </Provider>,
   document.getElementById('root')
