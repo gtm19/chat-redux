88955ba56c63a52fbc925bfe60892e8de75bbe78
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-05-16 13:20:24 +0100

    initialise message.jsx and message_list.jsx

---
+++ src/components/message.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Message = (props) => {
+  return(
+    <div>
+      Hello!
+    </div>
+  );
+};
+
+export default Message;
---
+++ src/containers/message_list.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+
+class MessageList extends Component { //eslint-disable-line
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default MessageList;
