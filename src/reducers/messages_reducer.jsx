ef23c3ff39432172287fe3e66818a99b3b9e851f
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-05-17 07:11:12 +0100

    add SEND_MESSAGE action to messages_reducer.jsx

--- src/reducers/messages_reducer.jsx
+++ src/reducers/messages_reducer.jsx
@@ -2,6 +2,11 @@ export default function(state = null, action) {
   switch (action.type) {
     case "GET_MESSAGES":
       return action.payload;
+    case "SEND_MESSAGE": {
+      const newMessages = state.slice(0);
+      newMessages.push(action.payload);
+      return newMessages;
+    }
     default:
       return state;
   }
